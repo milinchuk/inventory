2e63fcad00a64512013dc53c2cfa61cdac052a53
Anastasiia_Milinchuk <anastasia.milinchuk@gmail.com>
2020-09-08 19:29:36 +0300

    [refactoring]: get rid of old school repository queries

--- inventory-dl/src/main/java/inventory/dl/DrinkRepository.java
+++ inventory-dl/src/main/java/inventory/dl/DrinkRepository.java
@@ -2,15 +2,14 @@ package inventory.dl;
 
 import inventory.dl.domain.Drink;
 import inventory.dl.domain.DrinkType;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(propagation = Propagation.MANDATORY)
-public interface DrinkRepository extends CrudRepository<Drink, String> {
+public interface DrinkRepository extends Repository<Drink, String> {
 
-    @Query(value = "SELECT d FROM Drink d WHERE d.drinkType = :drinkType")
-    Drink findByDrinkType(@Param("drinkType") DrinkType drinkType);
+    Drink findByDrinkType(DrinkType drinkType);
+
+    Drink save(Drink drink);
 }
